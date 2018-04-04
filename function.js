8c6122e043ab5e421930efdd7cf7a2aebad4d828
Regallia <38038568+Regallia@users.noreply.github.com>
2018-04-05 01:10:35 +0700

    new

--- function.js
+++ function.js
@@ -35,3 +35,21 @@ $(window).load(function() { $(".preload-wrapper").fadeOut("slow"); })
     })
 
 
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+
+    $('a').click(function(){
+    $('html, body').animate({
+        scrollTop: $( $(this).attr('href') ).offset().top
+    }, 500);
+    return false;
+});
+
+
