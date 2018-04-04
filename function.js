3994841b4b7b445278bbe16afcb883b77e5437d0
LuteHend3 <38037477+LuteHend3@users.noreply.github.com>
2018-04-04 16:37:49 +0700

    Fanatic Finale Sticky
    Memek dora

--- function.js
+++ function.js
@@ -1,5 +1,16 @@
-$(window).scroll(function() {
-	var scrollTop = $(this).scrollTop();
-	$('.header-bg').css('top', -(scrollTop + 4) + 'px')
-})
+$(window).scroll(function() 
+	{
+		var scrollTop = $(this).scrollTop();
+		$('.header-bg').css('top', -(scrollTop + 4) + 'px')
+	})
+
 $(window).load(function() { $(".preload-wrapper").fadeOut("slow"); })
+
+$(window).on('scroll', function() {
+	if($(window).scrollTop()) 
+		{
+			$('nav').addClass('black');
+		}else{
+			$('nav').removeClass('black');
+		}
+	})
