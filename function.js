469b766ba1516d27d0396f9eb0aa81ea3f19e1f6
Andrew Patrick de Fretes <andrewbelanda@gmail.com>
2018-04-05 01:58:46 +0700

    smooth fix

--- function.js
+++ function.js
@@ -1,3 +1,6 @@
+
+
+
 //parallax
 $(window).scroll(function() 
     {
@@ -35,15 +38,20 @@ $(window).load(function() { $(".preload-wrapper").fadeOut("slow"); });
     });
 
 
-    $('a[href^="#"]').on('click', function(event) {
-        var target = $(this.getAttribute('href'));
-        if( target.length ) {
-            event.preventDefault();
-            $('html, body').stop().animate({
-                scrollTop: target.offset().top
-            }, 1000);
-        }
-    });
+$(function() {
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
 
     
 
