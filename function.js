a0e064f7ba8ff8951857cb6f894bb84d0e6ff66f
LuteHend3 <38037477+LuteHend3@users.noreply.github.com>
2018-04-04 13:10:46 +0700

    nav kas seles

--- function.js
+++ function.js
@@ -2,3 +2,4 @@ $(window).scroll(function() {
 	var scrollTop = $(this).scrollTop();
 	$('.header-bg').css('top', -(scrollTop + 4) + 'px')
 })
+$(window).load(function() { $(".preload-wrapper").fadeOut("slow"); })
