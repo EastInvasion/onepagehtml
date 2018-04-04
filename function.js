6c04fd65fb63bce6becb1b86fbc8ef5b92530e7e
Regallia <38038568+Regallia@users.noreply.github.com>
2018-04-04 21:17:39 +0700

    kerja babi

--- function.js
+++ function.js
@@ -1,11 +1,15 @@
+//parallax
 $(window).scroll(function() 
 	{
 		var scrollTop = $(this).scrollTop();
 		$('.header-bg').css('top', -(scrollTop + 4) + 'px')
 	})
 
+//preload
 $(window).load(function() { $(".preload-wrapper").fadeOut("slow"); })
 
+
+//stickynavbar
 $(window).on('scroll', function() {
 	if($(window).scrollTop()) 
 		{
@@ -14,3 +18,24 @@ $(window).on('scroll', function() {
 			$('nav').removeClass('black');
 		}
 	})
+
+
+// Menu-toggle button
+
+      $(document).ready(function() {
+            $(".menu-icon").on("click", function() {
+                  $("nav ul").toggleClass("showing");
+            });
+      });
+
+      // Scrolling Effect
+
+      $(window).on("scroll", function() {
+            if($(window).scrollTop()) {
+                  $('nav').addClass('black');
+            }
+
+            else {
+                  $('nav').removeClass('black');
+            }
+      })
