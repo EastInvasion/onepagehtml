e14cc5ca0ebd9db0beb82bf181afb765c1367ab2
LuteHend3 <38037477+LuteHend3@users.noreply.github.com>
2018-04-11 21:54:43 +0700

    smoothscroll

--- function.js
+++ function.js
@@ -1,42 +1,3 @@
-
-
-
-//parallax
-$(window).scroll(function() 
-    {
-        var scrollTop = $(this).scrollTop();
-        $('.header-bg').css('top', -(scrollTop + 4) + 'px')
-    });
-
-//preload
-$(window).load(function() { $(".preload-wrapper").fadeOut("slow"); });
-
-
-//stickynavbar
-    $(window).on('scroll', function() {
-        if($(window).scrollTop()) 
-            {
-                $('nav').addClass('black');
-            }else{
-                $('nav').removeClass('black');
-            }
-        });
-
-
-    $(document).ready(function(){
-        $(".menu-icon").on("click", function() {
-            $("nav ul").toggleClass("showing");
-         });
-    });
-
-    $(window).on("scroll", function() {
-        if($(window).scrollTop()) {
-            $('nav').addClass('black');
-        }else{
-            $('nav').removeClass('black');
-        }
-    });
-
 //smoothscroll
 $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
@@ -52,6 +13,3 @@ $(function() {
     }
   });
 });
-
-
-
